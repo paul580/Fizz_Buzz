117e2e90043e45bbeac70662b700ce42b968d695
Paul <paul18346@gmail.com>
2020-02-23 23:17:36 -0700

    Le dijimos al programa que imprimiera los numeros del 1 al 100 pero solo los divisibles entre 3 de manera exacta dijiera Fizz y los que fueran entre 5 dijiera Buzz y entre los dos FizzBuzz

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+
+namespace Fizz_Buzz
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            for(int Numero = 1; Numero < 100; Numero++)
+
+            {
+
+                if((Numero % 3) == 0 & (Numero % 5) == 0)
+
+                Console.WriteLine("FizzBuzz");
+
+                
+
+                else
+
+                 if((Numero % 3) == 0 )
+
+                 Console.WriteLine("Fizz");
+
+            
+
+                 else
+
+                  if((Numero % 5) == 0 )
+
+                  Console.WriteLine("Buzz");
+
+                  
+
+                  else
+
+                   Console.WriteLine(Numero);
+
+
+
+
+
+
+
+            
+
+            }
+
+
+
+        }
+
+    }
+
+}
